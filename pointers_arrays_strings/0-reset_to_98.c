34cdf615d9f5374d813af8b3773af98866689ed1
Vikdyp <victor@roubin.net>
2025-11-26 18:14:41 +0100

    Add function to reset integer value to 98 and header file

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the int pointed to to 98
+ * @n: pointer to the int to update
+ *
+ * Return: nothing (void)
+ */
 void reset_to_98(int *n)
 {
     *n = 98;
