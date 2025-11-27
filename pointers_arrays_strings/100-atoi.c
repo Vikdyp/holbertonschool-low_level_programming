2f5449566c909b8ff4fd7672ebd87414398218ad
Vikdyp <victor@roubin.net>
2025-11-27 18:14:00 +0100

    Fix formatting and indentation for consistency in _atoi function

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,33 +8,32 @@
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    int result = 0;
-    int started = 0;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int started = 0;
 
-    /* 1. Parcourir tous les caractères */
-    while (s[i] != '\0')
-    {
-        /* Compter les signes AVANT les chiffres */
-        if (s[i] == '-' && !started)
-            sign = sign * -1;
-        else if (s[i] == '+' && !started)
-            ; /* rien à faire */
+	/* Parcourir tous les caractères */
+	while (s[i] != '\0')
+	{
+		/* Compter les signes avant les chiffres */
+		if (s[i] == '-' && !started)
+			sign = sign * -1;
+		else if (s[i] == '+' && !started)
+			; /* rien à faire */
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (started)
+		{
+			/* Si on a commencé le nombre et qu'on tombe sur autre chose */
+			break;
+		}
 
-        /* Si on tombe sur un chiffre */
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            started = 1;
-            result = result * 10 + (s[i] - '0');
-        }
+		i++;
+	}
 
-        /* Si on a commencé les chiffres et qu'on tombe sur autre chose → stop */
-        else if (started)
-            break;
-
-        i++;
-    }
-
-    return (result * sign);
+	return (result * sign);
 }
